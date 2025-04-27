260b139b5e3ed68162ce751b584e5779634d54be
petrudemian <petredemi@gmail.com>
2025-04-27 11:22:55 +0100

    added links to each message through open button

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ let d = new Date()
 let added = d.getHours() + ':' + d.getMinutes() + '   ' + d.getDate() + '.' + month[d.getMonth()] + '.' + d.getFullYear()
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: added
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: added
@@ -27,9 +29,6 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
-//app.use("/new", newRouter)
-//app.use("/", indexRouter);
-
 app.get("/", (req, res,) => {
   res.render('index', {title: 'mini messageboard', messages: messages});
   });
@@ -40,17 +39,37 @@ app.get("/new", (req, res,) => {
 
 app.post("/new", (req, res) => {
   if(req.body.messagetext != '' && req.body.messageuser){
-    messages.push({text: req.body.messagetext, user: req.body.messageuser, added: added});
+    messages.push({id: req.body.id, text: req.body.messagetext, user: req.body.messageuser, added: added});
   }
     res.redirect("/")
   })
+  async function bbb(idd){
+    return messages.find((message) => message.id === idd)
+  }
+  
+async function aaa(req, res){
+    let {messageId} = req.params;
+    let message = await bbb(Number(messageId));
+    console.log(typeof message)
+    console.log(message)
+    res.render('user', {title: 'users', user: message.user,
+      text: message.text, added: message.added
 
+    });
+  }
 
-app.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
+  app.get("/:messageId", aaa)
 
+// app.get("/:messageId", (req, res) =>{
+//   const { messageId } = req.params;
+//   let message = bbb(Number(messageId))
+//   res.render('user', {title: 'users', messnr: messageId, message: message.user});
+// });
+  
+//app.get("/:authorId", (req, res) => {
+  //const { authorId } = req.params;
+ // res.send(`Author ID: ${authorId}`);
+//});
 app.use((req, res, next) => {
   throw new Error("OH NO!");
   // or next(new Error("OH NO!"));
